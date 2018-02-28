30c3b695434f43bbdc4ba906be626ced51b487b2
ajilderda <mail@andrejilderda.nl>
2018-02-28 15:00:15 +0100

    Restore selection after selectionChanged #46

--- Paddy.sketchplugin/Contents/Sketch/main.js
+++ Paddy.sketchplugin/Contents/Sketch/main.js
@@ -371,6 +371,7 @@ function selectionChanged(context) {
           }
         }
       }
+      layer.select_byExpandingSelection(true,true); // restore the new selection
     })
 
     log('Initial selected Props', JSON.stringify(initialSelectedProps))
